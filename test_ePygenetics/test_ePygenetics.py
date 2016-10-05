6b6501189e4dc05ce6af197243dff9b2b901ab19
CallumChalmers29 <ccha939@aucklanduni.ac.nz>
2016-10-05 21:37:18 +1300

    Added test for add cell line functions

--- test_ePygenetics/test_ePygenetics.py
+++ test_ePygenetics/test_ePygenetics.py
@@ -13,16 +13,16 @@ def test_user_input_unexpected2():
 	assert check_user_input(["1", 2, "3", 4], 3) == 0
 
 def test_find_snp_in_database():
-	assert find_snp('10', '103284', 'IMR90rep2-DNAse-hypersensitivity-GSM468801.wig') == "4"
+	assert find_snp('11', '73150080', 'IMR90rep2-DNAse-hypersensitivity-GSM468801.wig') == "131"
 
 def test_find_snp_in_database1():
-	assert find_snp("10", "80018084", 'IMR90rep2-DNAse-hypersensitivity-GSM468801.wig') == "45"
+	assert find_snp("11", "73150005", 'IMR90rep2-DNAse-hypersensitivity-GSM468801.wig') == "153"
 
 def test_find_snp_not_in_database():
 	assert find_snp('10', '49000', 'IMR90rep2-DNAse-hypersensitivity-GSM468801.wig') == "NaN"
 
 def test_find_snp_not_in_database1():
-	assert find_snp("10", "53424", 'IMR90rep2-DNAse-hypersensitivity-GSM468801.wig') == "NaN"
+ 	assert find_snp("10", "53424", 'IMR90rep2-DNAse-hypersensitivity-GSM468801.wig') == "NaN"
 
 def test_generate_cell_line_file_list():
         assert generate_cell_line_file_list(['IMR90rep2' , 'CD34+']) == ['IMR90rep2-DNAse-hypersensitivity-GSM468801.wig' , 'CD34+-DNAse-Hypersensitivity-DS12734.wig']
