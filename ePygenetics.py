dd1912c188d0b40dc6fb91a839b8491b6aa807ca
CallumChalmers29 <ccha939@aucklanduni.ac.nz>
2016-10-23 21:06:49 +1300

    Removed obsolete csv package import

--- ePygenetics.py
+++ ePygenetics.py
@@ -8,7 +8,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 import sys #import statements
 import os
 import glob
-import csv
 from itertools import islice
 from colorama import Fore, Style
 
